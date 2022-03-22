bd8f228be66b3664959e06bfefe2dd8f664ef493
Jia Jing <jiajing19932@gmail.com>
2022-03-22 21:15:24 +0800

    integrate wallet button

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,8 +1,18 @@
 <script setup>
-import { useRoute } from 'vue-router'
-import TheSidebar from './components/TheSidebar'
+    import { useRoute } from 'vue-router'
+    import TheSidebar from './components/TheSidebar'
 
-const route = useRoute()
+    import { PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets'
+    import { initWallet } from 'solana-wallets-vue'
+
+    const route = useRoute()
+
+    const wallets= [
+        new PhantomWalletAdapter,
+        new SolflareWalletAdapter
+    ]
+
+    initWallet({ wallets, autoConnect: true })
 </script>
 
 <template>
