b8c020e9e346a039f5d0165f8f92a497dedacbb7
Jia Jing <jiajing19932@gmail.com>
2022-04-10 16:08:02 +0800

    setup environment for deployment

--- app/src/composables/useWorkspace.js
+++ app/src/composables/useWorkspace.js
@@ -3,17 +3,20 @@ import { useAnchorWallet } from 'solana-wallets-vue';
 import { Connection, PublicKey } from '@solana/web3.js';
 import { Provider, Program } from '@project-serum/anchor';
 import idl from '../../../target/idl/solana_twitter.json';
+import idl from '@/idl/solana_twitter.json';
 
 const preflightCommitment = 'processed';
 const commitment = 'processed';
 const programID = new PublicKey(idl.metadata.address);
 let workspace = null;
 
+const clusterURL = process.env.VUE_APP_CLUSTER_URL;
+
 export const useWorkspace = () => workspace;
 
 export const initWorkspace = () => {
   const wallet = useAnchorWallet();
-  const connection = new Connection('http://127.0.0.1:8899', commitment);
+  const connection = new Connection(clusterURL, commitment);
   const provider = computed(
     () =>
       new Provider(connection, wallet.value, {
